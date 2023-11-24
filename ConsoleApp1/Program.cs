04fbb003fc1e04357204f97d33c96ea0a10b128e
Zbiku <Zbiku@MSI>
2023-11-24 22:41:04 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿class zad
+{
+    static double temp(double x)
+    {
+        return (5.0 / 9 * (x - 32));
+    }
+    static void Main(string[] args)
+    {
+        double x;
+        Console.WriteLine("Podaj temperaturę w stopniach Fahrenheita: ");
+        x = double.Parse(Console.ReadLine());
+        Console.WriteLine("Twoja temperaturę w stopniach Celciusza: ");
+        Console.WriteLine(temp(x));
+        Console.ReadKey();
+    }
+}
